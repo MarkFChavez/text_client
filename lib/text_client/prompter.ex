ffd6577390bfcd0c02588d8ab63e55b98fd29637
Mark <markchav3z@gmail.com>
2017-08-07 19:31:18 +0800

    initial working commit

---
+++ lib/text_client/prompter.ex
@@ -0,0 +1,31 @@
+defmodule TextClient.Prompter do
+
+  alias TextClient.State
+  
+  def accept_move(game = %State{}) do
+    IO.gets("Your guess: ")
+    |> check_input(game)
+  end
+
+  def check_input({:error, reason}, _) do
+    IO.puts("Game ended: #{reason}")
+    exit(:normal)
+  end
+
+  def check_input(:eof, _) do
+    IO.puts("Looks like you gave up...")
+    exit(:normal)
+  end
+
+  def check_input(input, game) do
+    input = String.trim(input)
+    cond do
+      input =~ ~r/\A[a-z]\z/ ->
+        Map.put(game, :guessed, input)
+      true ->
+        IO.puts "please enter a single lowercase letter"
+        accept_move(game)
+    end
+  end
+
+end
